1996539f1073e8c1f0015341a6ba055a36ba7499
godfrey8000 <godfrey8000@hotmail.com>
2023-05-28 22:07:49 +0800

    change response of token to body add swagger token description to employee api

--- src/main/java/jp/co/axa/apidemo/dto/AuthResponse.java
+++ src/main/java/jp/co/axa/apidemo/dto/AuthResponse.java
@@ -6,9 +6,10 @@ import lombok.Data;
 public class AuthResponse {
 
     private String token;
-
-    public AuthResponse(String token)
+    private String expireDay;
+    public AuthResponse(String token, String expireDay)
     {
         this.token = token;
+        this.expireDay = expireDay;
     }
 }
